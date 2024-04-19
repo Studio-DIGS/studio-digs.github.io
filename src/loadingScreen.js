1a7fbba6308bdb87596aeeeb130927d7a0a48bc7
Alex Ngo <42555152+alngo1@users.noreply.github.com>
2024-04-19 12:06:37 -0700

    some css modifications and prepping team page

--- src/loadingScreen.js
+++ src/loadingScreen.js
@@ -1,15 +1,29 @@
 export function loading(){
-	let loading = document.querySelector(".loading");
-	let monkey = document.querySelector(".monkey-gif");
-	window.addEventListener("load", () =>{
-		loading.style.opacity = "0";
-		loading.style.zIndex = "-1";
-		
-		monkey.style.zIndex = "0";
-		monkey.style.width = "50%";
-		monkey.style.margin = "0";
-		monkey.style.transition = "all 0.7s ease";
-	})
-	
+	// https://developer.mozilla.org/en-US/docs/Web/API/Document/readyState
+	document.addEventListener("readystatechange", (event) => {
+		if (event.target.readyState === "loading") {
+			let loading = document.querySelector(".loading");
+			let monkey = document.querySelector(".monkey-gif");
+			let body = document.querySelector("body");
+			loading.style.opacity = "1";
+			loading.style.zIndex = "1";
+			
+			monkey.style.zIndex = "1";
+			monkey.style.width = "75%";
+
+			body.style.overflowY = "hidden";
+		} else if (event.target.readyState === "complete") {
+			let loading = document.querySelector(".loading");
+			let monkey = document.querySelector(".monkey-gif");
+			let body = document.querySelector("body");
+			loading.style.opacity = "0";
+			loading.style.zIndex = "-1";
+			
+			monkey.style.zIndex = "0";
+			monkey.style.width = "50%";
+
+			body.style.overflowY = "visible";
+		}
+	  });
 }
 
